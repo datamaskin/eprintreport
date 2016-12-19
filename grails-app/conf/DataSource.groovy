0a93d67d95b93332a8c80233dec93dc00875a98c
datamaskinaggie <datamaskinaggie@tamu.edu>
2016-12-19 09:09:54 -0600

    Set hibernate.show_sql = true to obtain SQL queries as logs.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,10 +1,10 @@
-dataSource {
+/*dataSource {
     pooled = true
     jmxExport = true
     driverClassName = "org.h2.Driver"
     username = "sa"
     password = ""
-}
+}*/
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
@@ -20,6 +20,9 @@ environments {
         dataSource {
             /*dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"*/
+        }
+        hibernate {
+            show_sql = true
         }
     }
     test {
