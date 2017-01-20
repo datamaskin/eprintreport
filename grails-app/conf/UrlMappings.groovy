5a8c2aa2b4ae60b23af61c74f9cec2d9c116bf6c
datamaskinaggie <datamaskinaggie@tamu.edu>
2017-01-20 08:34:16 -0600

    Added maps for: gwrptsSeqNameBlob, gwrptsBlob, gwrptsBlobBytes

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -16,5 +16,18 @@ class UrlMappings {
             controller = "gwRpts"
             action = "gwrptsToJSON"
         }
+        "/gwrptsSNB" {
+            controller = "gwRpts"
+            action = "gwrptsSeqNameBlob"
+        }
+        "/gwrptsBlob" {
+            controller = "gwRpts"
+            action = "gwrptsBlob"
+        }
+
+        "/gwrptsBB" {
+            controller = "gwRpts"
+            action = "gwrptsBlobBytes"
+        }
 	}
 }
