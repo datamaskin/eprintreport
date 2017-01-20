6296bcb3c629a89fbb2eafcfabadc4dc7a7a4060
datamaskinaggie <datamaskinaggie@tamu.edu>
2017-01-20 08:29:39 -0600

    2 tests: Write PDF Blob data as PDF disk file and Test Service method getData() to populate GwRpts domain.

--- test/integration/edu/tamu/banner/eprintreport/CompassReportsTestIntegrationSpec.groovy
+++ test/integration/edu/tamu/banner/eprintreport/CompassReportsTestIntegrationSpec.groovy
@@ -1,14 +1,11 @@
 package edu.tamu.banner.eprintreport
 
 import grails.converters.JSON
-import grails.plugins.rest.client.RestBuilder
-import grails.plugins.rest.client.RestResponse
+import grails.plugin.cache.web.filter.PageFragmentCachingFilter
 import grails.test.spock.IntegrationSpec
-import groovy.json.JsonSlurper
-import org.apache.log4j.Logger
-import java.util.logging.Logger
-import org.codehaus.groovy.grails.web.json.JSONObject
+import groovy.json.JsonOutput
 
+import java.util.logging.Logger
 /**
  * Created by datamaskinaggie on 10/31/16.
  */
@@ -42,7 +39,7 @@ class CompassReportsTestIntegrationSpec extends IntegrationSpec {
     /*void "Fetch domain GwRptsDef properties from the REST endpoint" () {
         given:
         GwRptsDef rptsDef = new GwRptsDef()
-        rptsDef.gwRptsDefObjectName = "GURPDED"
+        rptsDef.gwRptsDefObjectName = "GZRTCCV"
 
         RestBuilder restBuilder = new RestBuilder()
 
@@ -51,54 +48,14 @@ class CompassReportsTestIntegrationSpec extends IntegrationSpec {
         }
 
         when:
-        RestResponse response = restBuilder.get("http://localhost:8080/EprintReport/GwRptsDef") {
-            contentType "application/vnd.org.jfrog.artifactory.security.Group+json"
-            accept JSONObject, 'application/json'
-        }
-
-        response.json instanceof JSONObject
-//        def list = new JsonSlurper().parseText(response.text)
-        response.json.gwRptsDefObjectName == 'GURPDED'
-
-        *//*def result = list["gwRptsDefObjectName"]
-
-        List names = new ArrayList()
-        names.addAll(result)
-
-        names.each {
-            println(it)
-        }*//*
-
-        then:
-        assert response.status == 200
-        assert response.json != null
-        assert response.text != null
-        assert result != null
-        assert names != null
-        assert response.text.size() > 0
-
-    }*/
-
-    /*void "Fetch domain GwRpts properties from the REST endpoint" () {
-        given:
-        GwRpts rpts = new GwRpts()
-        rpts.gwRptsSequence = 55
-
-        RestBuilder restBuilder = new RestBuilder()
-
-        restBuilder.restTemplate.messageConverters.removeAll {
-            it.class.name == 'org.springframework.http.converter.json.GsonHttpMessageConverter'
-        }
-
-        when:
-        RestResponse response = restBuilder.get("http://localhost:8080/EprintReport/GwRpts") {
+        RestResponse response = restBuilder.get("http://localhost:8080/eprint/GwRptsDef") {
             contentType "application/vnd.org.jfrog.artifactory.security.Group+json"
             accept JSONObject, 'application/json'
         }
 
         def list = new JsonSlurper().parseText(response.text)
 
-        def result = list["gwRptsSequence"]
+        def result = list["gwRptsDefObjectName"]
 
         List names = new ArrayList()
         names.addAll(result)
@@ -117,38 +74,93 @@ class CompassReportsTestIntegrationSpec extends IntegrationSpec {
 
     }*/
 
-    void "Fetch GwRpts reports in a join with GwRptsDef" () {
+    void "Fetch GwRpts reports using the report name" () {
         when:
-        def name = 'GURPDED'
+        def name = 'TGPHOLD'
 
         then:
         name != null
-        def reports = compassReportsService.getCompassReports(name)
-//        Logger.getLogger("CompassReportsService").info("Reports: " + reports.get(0).toString())
+        def reports = compassReportsService.getCompassReportsAsJSON(name)
         Logger.getLogger("CompassReportsService").info("Reports: " + reports)
-        println reports
+
+        reports.each {
+//            println "Report: ${it.get('GW_RPTS_BLOB') as JSON}" // blows up
+            println "Report: ${it.writer}"
+        }
     }
 
     void "Fetch GwRpts blobs using hibernate native SQL query" () {
         when:
-        BigDecimal seq = 44
+        BigInteger seq = 364
 
         then:
         seq != null
-        def report = compassReportsService.getGwRptsBlob(seq)
-        Logger.getLogger("CompassReportsService").info("Blob: " + report)
-        println report
+        def report = compassReportsService.getGwRptsBlobAsJSON(seq)
+        Logger.getLogger("CompassReportsService").info("Blob: " + report.get(0))
+
+        report.each {
+            println "Report: ${it.values()}"
+        }
 
     }
 
-    void "Fetch Student PIDM with UIN" () {
+    void "Fetch GwRpts blob bytes as byte array using hibernate native SQL query" () {
         when:
-        def uin = '826000013'
+        BigInteger seq = 364
 
         then:
-        uin != null
-        def pidm = compassReportsService.getStudentPidmUIN(uin)
-        Logger.getLogger("PIDM: " + pidm)
+        seq != null
+        def report = compassReportsService.getGwRptsBlobPDFBytes(seq)
+        Logger.getLogger("CompassReportsService").info("Byte array: " + report.get(0))
+
+        report.each {
+            println "Report: ${it}"
+        }
     }
 
+    void "Write PDF Blob data as PDF disk file" () {
+        when:
+        BigInteger seq = 364
+
+        then:
+        seq != null
+        def report = compassReportsService.writeBlobToFile(seq, "tgphold_seq_364.pdf", "/Users/datamaskinaggie/tmp/Blob", true)
+        Logger.getLogger("CompassReportsService").info("Report Blob: " + report.byteValue())
+        report.each {
+            println "Report PDF file disk write: ${it}"
+        }
+    }
+
+    void "Test Service method getData() to populate GwRpts domain" () {
+        when:
+        final String query = """
+                                select gw_rpts_object_name, gw_rpts_blob
+                                from gw_rpts inner join gw_rpts_def
+                                on gw_rpts.gw_rpts_sequence = '364'
+                              """
+        final GwRpts gwRpts = new GwRpts()
+
+        then:
+        query != null
+        def report = compassReportsService.getData(query, gwRpts.class)
+
+        report.each {
+            println "Report: ${it}"
+        }
+    }
+
+    void "Test fetching Blobs as byte[] array" () {
+        when:
+        final BigInteger seq = 364
+
+        then:
+        seq != null
+        def report = compassReportsService.getGwRptsBlobBytes(seq)
+        Logger.getLogger("CompassReportsService").info("Report Blob: " + report.toList())
+
+        /*report.each {
+            println "Report: ${it.toString()}"
+        }*/
+        report.toList()
+    }
 }
