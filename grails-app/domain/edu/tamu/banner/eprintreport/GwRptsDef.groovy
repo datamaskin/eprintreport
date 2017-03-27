d4465195f9770093852af057a296a1612f817c4d
datamaskinaggie <datamaskinaggie@tamu.edu>
2017-03-27 14:42:00 -0500

    Refactored all properties to List

--- grails-app/domain/edu/tamu/banner/eprintreport/GwRptsDef.groovy
+++ grails-app/domain/edu/tamu/banner/eprintreport/GwRptsDef.groovy
@@ -2,13 +2,22 @@ package edu.tamu.banner.eprintreport
 
 class GwRptsDef {
 
-	String gwRptsDefObjectName
-	String gwRptsDefObjectDesc
-	String gwRptsDefMaintainedDept
-	String gwRptsDefMaintainedColl
-	Integer gwRptsDefRetentionDays
-	String gwRptsDefUserid
-	Date gwRptsDefActivityDate
+	static mapWith = "none"
+
+//	String gwRptsDefObjectName
+//	String gwRptsDefObjectDesc
+//	String gwRptsDefMaintainedDept
+//	String gwRptsDefMaintainedColl
+//	Integer gwRptsDefRetentionDays
+//	String gwRptsDefUserid
+//	Date gwRptsDefActivityDate
+	List<String> gwRptsDefObjectName = []
+	List<String> gwRptsDefObjectDesc = []
+	List<String> gwRptsDefMaintainedDept = []
+	List<String> gwRptsDefMaintainedColl = []
+	List<Integer> gwRptsDefRetentionDays = []
+	List<String> gwRptsDefUserid = []
+	List<Date> gwRptsDefActivityDate = []
 
 	static mapping = {
 		id name: "gwRptsDefObjectName", generator: "assigned"
