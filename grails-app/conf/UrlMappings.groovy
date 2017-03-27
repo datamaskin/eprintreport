dc7277eb5a325ba6b75e9523450b0abbbc4d4c7f
datamaskinaggie <datamaskinaggie@tamu.edu>
2017-03-27 14:45:21 -0500

    Partial restoration of the mappings to include the accordion buttons events.

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,20 +7,26 @@ class UrlMappings {
             }
         }
 
-        "/EprintReport"(resources: "gwRptsDef")
+//        "/EprintReport"(resources: "gwRptsDef")
+        "/"(view:"/index")
 //        "/"(view:"index")
-        "/"(controller: "gwRptsDef", action: "index")
+//        "/"(controller: "gwRptsDef", action: "index")
+//        "/"(controller: "gwRptsDef", action: "getReportDefJSON")
+//        "/"(controller: "gwRptsDef", action: "getReportDefJSON_H2")
         "500"(view:'/error')
-
-        "/gwRpts" {
+        "/gwRptsDefJSON_H2" {
+            controller = "gwRptsDef"
+            action = "gwrptsReportDefJSON_H2"
+        }
+        /*"/gwRpts" {
             controller = "gwRpts"
             action = "gwrptsToJSON"
-        }
+        }*/
         "/gwrptsSNB" {
             controller = "gwRpts"
             action = "gwrptsSeqNameBlob"
         }
-        "/gwrptsBlob" {
+        /*"/gwrptsBlob" {
             controller = "gwRpts"
             action = "gwrptsBlobAsByte"
         }
@@ -48,52 +54,13 @@ class UrlMappings {
             controller = "gwRpts"
             action = "getReportInfoJSON"
         }
-
-        // Mappings supported by resource-specific controllers
-        // should be added before the default mapping used for
-        // resources handled by the default RestfulApiController.
-
-        // name otherthingRestfulApi: "api/other-things/$id"
-
-        // Default controller to handle RESTful API requests.
-        // Place URL mappings to specific controllers BEFORE this mapping.
-        //
-        "/api/$pluralizedResourceName/$id"(controller:'restfulApi') {
-            action = [GET: "show", PUT: "update",
-                      DELETE: "delete"]
-            parseRequest = false
-            constraints {
-                // to constrain the id to numeric, uncomment the following:
-                // id matches: /\d+/
-            }
-        }
-        "/api/$pluralizedResourceName"(controller:'restfulApi') {
-            action = [GET: "list", POST: "create"]
-            parseRequest = false
-        }
-
-        // Support for nested resources. You may add additional URL mappings to handle
-        // additional nested resource requirements.
-        //
-        "/api/$parentPluralizedResourceName/$parentId/$pluralizedResourceName/$id"(controller:'restfulApi') {
-            action = [GET: "show", PUT: "update", DELETE: "delete"]
-            parseRequest = false
-            constraints {
-                // to constrain the id to numeric, uncomment the following:
-                // id matches: /\d+/
-            }
-        }
-
-        "/api/$parentPluralizedResourceName/$parentId/$pluralizedResourceName"(controller:'restfulApi') {
-            action = [GET: "list", POST: "create"]
-            parseRequest = false
-        }
-
-        // We can also expose 'query-with-POST' URLs by using a different prefix:
-        //
-        "/qapi/$pluralizedResourceName"(controller:'restfulApi') {
-            action = [GET: "list", POST: "list"]
-            parseRequest = false
+        "/reportDefJSON" {
+            controller = "gwRptsDef"
+            action = "getReportDefJSON"
+        }*/
+        "/showPeople" {
+            controller = "Dt"
+            action = "showPeople"
         }
 	}
 }
